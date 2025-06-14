6dd62c7050275bcad7e3997defef66f24dc330b0
M Sibaram Dora <msibaramdora2005@gmail.com>
2025-06-14 08:27:01 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': 'off', // disable the unused vars rule
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off', // disable the explicit any rule
     },
   },
 ]
